9c4b0f2c054460b2c9d0baee1d58f1612ad7ed6b
Ludwing Pérez <ludwing@ludwing-idei>
2019-03-05 12:14:10 -0600

    Ejemplo de uso de watchers

--- src/App.vue
+++ src/App.vue
@@ -18,6 +18,16 @@ export default {
     fullName() {
       return `${this.name} ${this.lastName}`;
     }
+  },
+  watch: {
+    //Se ejecuta código a partir de que una propiedad del view-model cambia
+    //No devuelven un valor, no son propiedades y no se pueden usar en expresiones.
+
+    //Se enlazan directamente con alguna variable del view-model, por lo tanto
+    //se deben tener el mismo nombre.
+    name(newValue, oldValue) {
+      console.log(newValue, oldValue);
+    }
   }
 }
 </script>
