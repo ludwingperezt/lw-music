92d16cbc64b62124d3d2733728b1f6f5dfa8277d
Ludwing Pérez <ludwing@ludwing-idei>
2019-03-05 16:28:17 -0600

    Layout y funcionalidad prototipo base para aplicación lw-music

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,61 @@
 <template lang="pug">
   #app
-    input(v-model="name")
-    //button(v-on:click="format") Format
-    button(@click="format") Format
-    p {{ formatName }}
+    section.section
+      nav.navbar.has-shadow
+        .navbar-item.is-expanded
+          .field.has-addons
+            .control.is-expanded
+              input.input.is-large(
+                type="text",
+                v-model="searchQuery",
+                placeholder="Buscar canciones"
+              )
+            .control
+              a.button.is-info.is-large(@click="search") Buscar
+            .control
+              a.button.is-danger.is-large &times;
+          p
+            small {{ searchMessage }}
+      .container.results
+        .columns(v-for="t in tracks")
+          .column {{ t.name }} {{ t.artist }}
 </template>
 
 <script>
+const tracks = [
+  { name: "Blue cheese", artist:"Courtney Barnett ft. Kurt Vile" },
+  { name: "Imagine", artist:"John Lennon" },
+  { name: "All you need is love", artist:"The Beatles" },
+  { name: "Time", artist:"Pink Floyd" },
+  { name: "The End", artist:"The Doors" },
+  { name: "She smiled sweetly", artist:"The Rolling Stones" },
+  { name: "All along the watchtower", artist:"Bob Dylan" }
+];
 export default {
   name: 'app',
   data () {
     return {
-      name: "",
-      formatName: ""
+      searchQuery: "",
+      tracks: []
+    }
+  },
+  computed:{
+    searchMessage() {
+      return `Encontrados ${this.tracks.length}`;
     }
   },
   methods: {
-    format() {
-      this.formatName = this.name.split(' ').join('-');
+    search() {
+      console.log(this.searchQuery);
+      this.tracks = tracks;
     }
   }
 }
 </script>
 
 <style lang="scss">
-  @import './scss/main.scss'
+  @import './scss/main.scss';
+  .results {
+    padding: 2em;
+  }
 </style>
