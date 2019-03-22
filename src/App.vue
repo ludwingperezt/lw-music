4701c08a32289e82250f66b0ff6b4e50ab2fd6d0
Ludwing Perez <mirage232@gmail.com>
2019-03-21 20:58:40 -0600

    Video 24: Componentes Se crearon los componentes para el header y footer de la app y se registraron de manera local.
    Notas:
    1.  En los archivos Footer.vue y Header.vue se eliminó la parte de css y javascript
        porque no se utilizan y así se evita sobrecargar la compilación con código que
        no se utiliza.
    2.  En App.vue se hizo la importación de los componentes header y footer bajo los
        nombres LmHeader y LmFooter respectivamente. Se hizo ésto porque es una buena
        práctica colocar un prefijo a los componentes personalizados para poder
        diferenciarlos de componentes de terceros, u otros elementos HTML.
    3.  Vue hace la traducción de nombres de PascalCase a kebab-case automáticamente,
        por ello los componentes fueron declarados en el objeto components con sintaxis
        PascalCase y dentro del HTML (pug en éste caso) se utilizaron con sus nombres
        en kebab-case.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template lang="pug">
   #app
+    lm-header
     section.section
       nav.navbar.has-shadow
         .navbar-item.is-expanded
@@ -21,13 +22,17 @@
         .columns(v-for="t in tracks")
           .column
             | {{ t.name }} - {{ t.artists[0].name }}
+    lm-footer
 </template>
 
 <script>
 import trackService from './services/track'
+import LmFooter from './components/layout/Footer.vue' //ES2015 convierte la sintaxis de PascalCase a kebab-case
+import LmHeader from './components/layout/Header.vue'
 
 export default {
   name: 'app',
+  components: { LmFooter, LmHeader },
   data () {
     return {
       searchQuery: "",
---
+++ src/components/layout/Header.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+  section.hero.is-success
+    .hero-head
+      header.navbar
+        .container
+          .nav-start
+            .nav-item
+              strong 📻 Lw Music
+          .nav-end.nav-menu
+    .hero-body
+      .container.has-text-centered
+        h1.title Lw Music
+        h2.subtitle Canciones que están Vue-nísimas
+</template>
